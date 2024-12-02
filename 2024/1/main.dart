3511629c5570fa2c263bd6f193ad54e8a62eb685
devhmac <devhmac@gmail.com>
2024-12-01 21:17:35 -0700

    2024 init

---
+++ 2024/1/main.dart
@@ -0,0 +1,17 @@
+// locations listed by ID
+// compare two lists
+// - pair up numbers and measure delta
+// - each num unique location
+
+// compare sorted lists (smallest v smallest)
+//sum delta
+import 'dart:io';
+
+String get input {
+  File input = new File('./exampleInput.txt');
+  return input.readAsStringSync();
+}
+
+main() async {
+  print(input);
+}
